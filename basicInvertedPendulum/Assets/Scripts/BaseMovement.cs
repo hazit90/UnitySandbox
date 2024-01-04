4b3ea48ab99bacbf454c94e84bb756a1e1c2e41e
hazit90 <hazit_90@hotmail.com>
2024-01-04 16:37:58 +0000

    added project for inverted pendulum

---
+++ basicInvertedPendulum/Assets/Scripts/BaseMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMovement : MonoBehaviour
+{
+    public float speed = 5.0f;  // Speed of the base movement
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); // 'A' and 'D' keys
+        float moveVertical = Input.GetAxis("Vertical");     // 'W' and 'S' keys
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
